ab1c8c125c785fb661cef933a1447a4332b7f2e2
Arthy <Arthy@ARTHY>
2021-03-04 23:54:00 +0530

    pranav idiot

--- myapplication/src/com/capg/app/Demo.java
+++ myapplication/src/com/capg/app/Demo.java
@@ -3,7 +3,9 @@ package com.capg.app;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("HEllo friends");
+		System.out.println("Hello friends");
+		System.out.println("Pranav is an idiotic fellow");
+		System.out.println("Goms is a sweet and cute girl");
 
 	}
 
