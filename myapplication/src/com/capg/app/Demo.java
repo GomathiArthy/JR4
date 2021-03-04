d3004eb1a6930e2452f906036d4b9498ffb6f7bc
GomathiArthy <69449921+GomathiArthy@users.noreply.github.com>
2021-03-05 00:04:34 +0530

    Pranavidiotupdate

--- myapplication/src/com/capg/app/Demo.java
+++ myapplication/src/com/capg/app/Demo.java
@@ -6,6 +6,8 @@ public class Demo {
 		System.out.println("Hello friends");
 		System.out.println("Pranav is an idiotic fellow");
 		System.out.println("Goms is a sweet and cute girl");
+		System.out.println("Pranav is born single and going to dei single even after 60yrs");
+	
 
 	}
 
